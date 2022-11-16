5819fabbcf352ec20d1a726cfa711eb69e2781ef
LuisaPurifica <rm95324@fiap.com.br>
2022-11-16 11:06:49 -0300

    Primeiro servlet criado + segundo servlet recebendo parametro

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundo.java
@@ -0,0 +1,29 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//oi
+@WebServlet(urlPatterns = "/oi")
+public class OiMundo extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<p>oi mundo, parabéns vc escreveu o primeiro servlet</p>");
+		out.println("</body>");
+		out.println("</html>");
+
+		System.out.println("O servlet OiMundoServlet foi chamado");
+
+	}
+
+}
