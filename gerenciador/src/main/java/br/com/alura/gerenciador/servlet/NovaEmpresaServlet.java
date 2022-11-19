0eb8726610ce24a62cc00f6584879f9c8e173755
LuisaPurifica <rm95324@fiap.com.br>
2022-11-19 17:02:09 -0300

    web.xml e deploy

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -5,7 +5,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundo.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundo.java
@@ -11,6 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 //oi
 @WebServlet(urlPatterns = "/oi")
 public class OiMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public OiMundo() {
+		System.out.println("Criado oi mundo servlet");
+	}
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
