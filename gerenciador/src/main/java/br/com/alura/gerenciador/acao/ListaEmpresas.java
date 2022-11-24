4473427cce472c6df74103d56e4b9287671ee4f6
LuisaPurifica <rm95324@fiap.com.br>
2022-11-24 10:46:47 -0300

    Web Services - retornar JSON e XML

--- gerenciador/src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -14,7 +14,6 @@ public class ListaEmpresas implements Acao {
 
 	public String executar(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-//		long antes = System.currentTimeMillis();
 
 		System.out.println("listando empresas");
 
@@ -23,10 +22,6 @@ public class ListaEmpresas implements Acao {
 		List<Empresa> empresas = banco.getEmpresas();
 
 		request.setAttribute("lista", empresas);
-
-//		long depois = System.currentTimeMillis();
-//
-//		System.out.println("Tempo de execução: " + (depois - antes));
 
 		return "forward:listaEmpresas.jsp";
 	}
