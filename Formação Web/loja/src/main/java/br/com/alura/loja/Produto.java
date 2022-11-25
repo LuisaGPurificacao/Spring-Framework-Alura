44765fc57eb2e4ccf32b20b9a9979dd52682bb05
LuisaPurifica <rm95324@fiap.com.br>
2022-11-25 15:20:24 -0300

    Build e proxy

--- Formação Web/loja/src/main/java/br/com/alura/loja/Produto.java
+++ Formação Web/loja/src/main/java/br/com/alura/loja/Produto.java
@@ -1,7 +1,31 @@
 package br.com.alura.loja;
 
+import java.math.BigDecimal;
+
 public class Produto {
 
-	
-	
+	private String nome;
+	private BigDecimal preco;
+
+	public Produto(String nome, BigDecimal preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
 }
--- Formação Web/loja/src/test/java/br/com/alura/loja/ProdutoTest.java
+++ Formação Web/loja/src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -1,14 +1,17 @@
 package br.com.alura.loja;
 
-import static org.junit.Assert.*;
+import java.math.BigDecimal;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Produto p = new Produto("teste", BigDecimal.TEN);
+		Assert.assertEquals("teste", p.getNome());
+		Assert.assertEquals(BigDecimal.TEN, p.getPreco());
 	}
 
 }
