fc22b7f5ec914ceb235bc0e2b0c4a7d52100edee
LuisaPurifica <rm95324@fiap.com.br>
2022-12-03 10:44:52 -0300

    Projecoes - FuncionarioDTO

--- Formação Banco/JPA/Spring Data JPA/spring-data/src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ Formação Banco/JPA/Spring Data JPA/spring-data/src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.orm.FuncionarioProjecao;
 
 @Repository
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
@@ -22,5 +23,8 @@ public interface FuncionarioRepository extends CrudRepository<Funcionario, Integ
 
 	@Query(value = "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data", nativeQuery = true)
 	List<Funcionario> findDataContratacaoMaior(LocalDate data);
+
+	@Query(value = "SELECT f.id, f.nome, f.salario FROM funcionarios f", nativeQuery = true)
+	List<FuncionarioProjecao> findFuncionarioSalario();
 
 }
--- Formação Banco/JPA/Spring Data JPA/spring-data/src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
+++ Formação Banco/JPA/Spring Data JPA/spring-data/src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.orm.FuncionarioProjecao;
 import br.com.alura.spring.data.repository.FuncionarioRepository;
 
 @Service
@@ -29,6 +30,7 @@ public class RelatoriosService {
 			System.out.println("1- Busca funcionario por nome");
 			System.out.println("2- Busca funcionario por nome, data contratacao e salario maior");
 			System.out.println("3- Busca funcionario por data contratacao");
+			System.out.println("4- Pesquisa funcionario salario");
 
 			int action = scan.nextInt();
 
@@ -43,6 +45,10 @@ public class RelatoriosService {
 
 			case 3:
 				buscaFuncionarioDataContratacao(scan);
+				break;
+
+			case 4:
+				pesquisaFuncionarioSalario();
 				break;
 
 			default:
@@ -81,6 +87,12 @@ public class RelatoriosService {
 
 		List<Funcionario> funcionarios = funcionarioRepository.findDataContratacaoMaior(localDate);
 		funcionarios.forEach(System.out::println);
+	}
+
+	private void pesquisaFuncionarioSalario() {
+		List<FuncionarioProjecao> funcionarios = funcionarioRepository.findFuncionarioSalario();
+		funcionarios.forEach(f -> System.out.println(
+				"Funcionarios: id: " + f.getId() + " | nome: " + f.getNome() + " | salario: " + f.getSalario()));
 	}
 
 }
