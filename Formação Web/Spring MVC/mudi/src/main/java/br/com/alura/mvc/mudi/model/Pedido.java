31aad054d4b4ec8860a3985a3bbb34ce0e228276
LuisaGPurifica <luisagabriele38@gmail.com>
2022-12-13 12:24:52 -0300

    API Rest

--- Formação Web/Spring MVC/mudi/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ Formação Web/Spring MVC/mudi/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -12,7 +12,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Pedido {
 
 	@Id
--- Formação Web/Spring MVC/mudi/src/main/java/br/com/alura/mvc/mudi/model/User.java
+++ Formação Web/Spring MVC/mudi/src/main/java/br/com/alura/mvc/mudi/model/User.java
@@ -9,8 +9,11 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "users")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class User {
 
 	@Id
