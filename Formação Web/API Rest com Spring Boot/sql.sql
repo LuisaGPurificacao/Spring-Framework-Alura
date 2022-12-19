f4d838e7a4d20589896009f58d6398cb2a227d31
LuisaGPurifica <luisagabriele38@gmail.com>
2022-12-19 15:54:00 -0300

    Spring Security

--- Formação Web/API Rest com Spring Boot/sql.sql
+++ Formação Web/API Rest com Spring Boot/sql.sql
@@ -4,3 +4,8 @@ use vollmed_api;
 
 select * from medicos;
 select * from pacientes;
+select * from usuarios;
+
+insert into usuarios values (1, 'ana.souza@voll.med', '$2a$10$Y50UaMFOxteibQEYLrwuHeehHYfcoafCopUazP12.rqB41bsolF5.');
+
+commit;
